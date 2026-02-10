69e354c219dcc2f7fdd25d55428fb8d0431095e8
Rajkumar <rajkumarkundu212@gmail.com>
2026-02-10 11:13:09 +0530

    first commit

---
+++ day-1/bacis.ts
@@ -0,0 +1,6 @@
+function greet(name : string) : string{
+    return `Hello ${name}`
+}
+
+console.log(greet("Rajkumar"));
+// console.log(greet(42));
---
+++ day-1/basic.js
@@ -0,0 +1,6 @@
+// function greet(name){
+//     return `Hello ${name}`
+// }
+
+// console.log(greet("Rajkumar"));
+// console.log(greet(42));
